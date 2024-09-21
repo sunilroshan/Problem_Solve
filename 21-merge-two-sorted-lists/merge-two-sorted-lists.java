f79c064ad2e1c61a48d71e01717c4456dd839cbf
Sunil Roshan A <117465891+sunilroshan@users.noreply.github.com>
2024-09-21 15:12:37 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (88.66%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -1,34 +1,56 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
 
         ListNode temp1 = list1;
         ListNode temp2 = list2;
 
-        ListNode dummy = new ListNode(-1);
-        ListNode cur = dummy; // It acts as a placeholder
+       // LinkedList ans = new LinkedList();//which are not part of the standard way to handle linked lists in Java.
 
-        // Merge lists while both have nodes
-        while(temp1 != null && temp2 != null) {
-            if(temp1.val <= temp2.val) {
+        //how in i<n likeie) temp != null
+
+        ListNode dummy = new ListNode(-1);
+        ListNode cur = dummy; // it is like temp and dummy is like a head
+
+        while(temp1 != null && temp2 != null){
+
+            if(temp1.val <= temp2.val){
                 cur.next = temp1;
                 temp1 = temp1.next;
-            } else {
+            }
+            else{
                 cur.next = temp2;
                 temp2 = temp2.next;
             }
-            cur = cur.next;
+
+          cur = cur.next;
+
         }
 
-        // If there are remaining nodes in list1, append them
-        if(temp1 != null) {
+        //if list1 1-2-3
+        //   list2 1-2-3-4-5
+
+        while(temp1 != null){
             cur.next = temp1;
+            temp1 = temp1.next;
+             cur = cur.next;
         }
-        
-        // If there are remaining nodes in list2, append them
-        if(temp2 != null) {
+        while(temp2 != null){
             cur.next = temp2;
+            temp2 = temp2.next;
+             cur = cur.next;
         }
 
-        return dummy.next; // Return the head of the merged list (skipping the dummy node)
+        return dummy.next; // Return the next of the dummy node, which is the head of the new list
+        
     }
 }

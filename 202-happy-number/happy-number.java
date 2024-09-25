16e674be2192446d4c4d335da1fc65c1619d16cf
Sunil Roshan A <117465891+sunilroshan@users.noreply.github.com>
2024-09-26 00:00:28 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (29.36%) - LeetSync

--- 202-happy-number/happy-number.java
+++ 202-happy-number/happy-number.java
@@ -1,41 +1,75 @@
 class Solution {
     public boolean isHappy(int n) {
 
-        HashSet<Integer> seen = new HashSet<>();
+        // HashSet<Integer> seen = new HashSet<>();
           
-         while(n != 1){ 
-            boolean found = false;
+        //  while(n != 1){ 
+        //     boolean found = false;
 
-            int sum = 0;
+        //     int sum = 0;
             
-            while(n != 0){
+        //     while(n != 0){
                
-                sum+= Math.pow(n%10,2);
-                n=n/10;
-            }
+        //         sum+= Math.pow(n%10,2);
+        //         n=n/10;
+        //     }
 
-            if(sum == 1){
+        //     if(sum == 1){
+        //         return true;
+                
+        //     }
+
+        //     n=sum;
+
+        //     if(seen.contains(sum)){
+        //         return false;
+                
+        //     }
+
+        //     seen.add(sum);
+
+        //     n = sum;
+
+
+
+        //  }
+
+        //  return true;
+
+
+
+        //you can solve this by cycle linked list because once visited and again it is visited
+
+        int fast = n;
+        int slow = n;
+
+      do{
+            fast = square(square(fast));
+            slow = square(slow);
+
+      }while(slow != fast);
+            
+
+            if(slow == 1){
                 return true;
-                
             }
-
-            n=sum;
-
-            if(seen.contains(sum)){
-                return false;
-                
-            }
-
-            seen.add(sum);
-
-            n = sum;
-
-
-
-         }
-
-         return true;
+            return false;       
 
     
+    }
+
+    public int square(int n){
+
+        int ans = 0;
+
+        while(n > 0){
+
+            int rem = n % 10;
+            ans += rem*rem;
+            n = n /10;
+
+        }
+
+        return ans;
     }
 }

f959c3ff33dcb8ca57990cef5442eed3fa1314c3
Sunil Roshan A <117465891+sunilroshan@users.noreply.github.com>
2024-09-29 15:00:23 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (94.94%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.java
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.java
@@ -11,26 +11,41 @@
 class Solution {
     public ListNode middleNode(ListNode head) {
 
-        ListNode temp = head;
+        //it is ok if the interviewer want to ask in only single pass
 
-        int size = 0;
+        // ListNode temp = head;
 
-        while(temp != null){
-            size++;
-            temp = temp.next;
+        // int size = 0;
+
+        // while(temp != null){
+        //     size++;
+        //     temp = temp.next;
+        // }
+
+        // temp = head;
+
+        // for(int i=0; i<size/2; i++){
+
+        //     temp = temp.next;
+
+        // }
+
+        // ListNode ans = temp;
+
+        // return ans;
+
+
+        //using fast and slow pointer
+
+        ListNode f = head;
+        ListNode s = head;
+
+        while(f != null && f.next != null){
+
+            f = f.next.next;
+            s = s.next;
         }
-
-        temp = head;
-
-        for(int i=0; i<size/2; i++){
-
-            temp = temp.next;
-
-        }
-
-        ListNode ans = temp;
-
-        return ans;
+        return s;
         
     }
 }

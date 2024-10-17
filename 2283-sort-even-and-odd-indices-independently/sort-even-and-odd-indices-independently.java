902d7b0bc44bc0b34756a8f494003d8ceabce092
Sunil Roshan A <117465891+sunilroshan@users.noreply.github.com>
2024-10-17 12:21:06 +0530

    Time: 4 ms (55.89%) | Memory: 44.5 MB (45.11%) - LeetSync

--- 2283-sort-even-and-odd-indices-independently/sort-even-and-odd-indices-independently.java
+++ 2283-sort-even-and-odd-indices-independently/sort-even-and-odd-indices-independently.java
@@ -2,7 +2,7 @@ class Solution {
     public int[] sortEvenOdd(int[] nums) {
         int n = nums.length;
 
-        for (int i = 0; i < n; i += 2) {
+        for (int i = 0; i < n - 2; i += 2) {
             for (int j = i + 2; j < n; j += 2) {
                 if (nums[i] > nums[j]) {
                     
@@ -14,7 +14,7 @@ class Solution {
         }
 
         
-        for (int i = 1; i < n; i += 2) {
+        for (int i = 1; i < n - 2; i += 2) {
             for (int j = i + 2; j < n; j += 2) {
                 if (nums[i] < nums[j]) {
                     int temp = nums[i];

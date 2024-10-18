141b4c3ad186eff8d19d024accd7b4737e2538d5
Sunil Roshan A <117465891+sunilroshan@users.noreply.github.com>
2024-10-18 14:18:36 +0530

    Time: 5 ms (53.27%) | Memory: 43 MB (75.44%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,13 +1,35 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         
-        char[] arr1 = s.toCharArray();
-        char[] arr2 = t.toCharArray();
+        // char[] arr1 = s.toCharArray();
+        // char[] arr2 = t.toCharArray();
 
-        Arrays.sort(arr1);
-        Arrays.sort(arr2);
+        // Arrays.sort(arr1);
+        // Arrays.sort(arr2);
 
-        return Arrays.equals(arr1,arr2); // it returns as false or true
-        
+        // return Arrays.equals(arr1,arr2); // it returns as false or true
+
+        //efficient approach
+
+        //O(n)
+
+        if(s.length() != t.length()){
+            return false;
+        }
+
+        int arr[] = new int[26];
+
+        for(int i = 0; i < s.length(); i++){
+            arr[s.charAt(i) - 'a']++;
+            arr[t.charAt(i) - 'a']--;
+        }
+
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] != 0){
+                return false;
+            }
+        }
+
+        return true;        
     }
 }

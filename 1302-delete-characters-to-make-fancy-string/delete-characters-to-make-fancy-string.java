d04a9c133ba3e2ec23113540eb6250245c0ba1f4
Sunil Roshan A <117465891+sunilroshan@users.noreply.github.com>
2024-11-01 21:35:13 +0530

    Time: 40 ms (41.86%) | Memory: 45.6 MB (47.62%) - LeetSync

--- 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.java
+++ 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.java
@@ -1,28 +1,45 @@
 class Solution {
     public String makeFancyString(String s) {
 
-       if(s.length() < 3){
-        return s;
-       }
+    //    if(s.length() < 3){
+    //     return s;
+    //    }
+    //     StringBuilder sb = new StringBuilder();
+
+    //     for(int i = 0; i < s.length() - 2; i++){
+    //         if((s.charAt(i) != s.charAt(i+1)) || (s.charAt(i) != s.charAt(i+2))){
+    //              sb.append(s.charAt(i));
+    //         }
+    //     }
+
+    //     // if(s.length() )
+
+        
+    //     sb.append(s.charAt(s.length() - 2));
+    //     sb.append(s.charAt(s.length() - 1));
+
+        
+
+
+    //     String ans = sb.toString();
+    //     return ans;
+
+    //good approach but use this
+
+     int n = s.length();
         StringBuilder sb = new StringBuilder();
-
-        for(int i = 0; i < s.length() - 2; i++){
-            if((s.charAt(i) != s.charAt(i+1)) || (s.charAt(i) != s.charAt(i+2))){
-                 sb.append(s.charAt(i));
-            }
+        if(n < 3){
+            return s;
         }
-
-        // if(s.length() )
-
         
-        sb.append(s.charAt(s.length() - 2));
-        sb.append(s.charAt(s.length() - 1));
-
-        
-
-
-        String ans = sb.toString();
-        return ans;
+            sb.append(s.charAt(0));
+            sb.append(s.charAt(1));
+            for(int i=2; i<n; i++){
+                if(s.charAt(i) != s.charAt(i-1) || s.charAt(i) != s.charAt(i-2)){
+                    sb.append(s.charAt(i));
+                }
+            }
+            return sb.toString();
         
     }
 }

6fe28d42d564e97a2caa888a353d0112a6554a7b
Sunil Roshan A <117465891+sunilroshan@users.noreply.github.com>
2025-02-03 20:25:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Sum 1 to n Divisors/sum-1-to-n-divisors.java
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String args[]) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int N = sc.nextInt();
+            Solution ob = new Solution();
+            int ans = ob.sumOfDivisors(N);
+            System.out.println(ans);
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+class Solution {
+    public static int sumOfDivisors(int n) {
+   
+        int sum = 0;
+        
+        for(int i = 1; i <= n; i++){
+            
+            int num = i;
+            
+            for(int j = 1; j <= num; j++){
+               
+                if(num % j == 0){
+                    sum += j;
+                }
+            }
+        }
+        
+        return sum;
+    }
+}

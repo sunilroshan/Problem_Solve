9f7b19fb18ff4b5443e8a8a23623243ed30ba56e
Sunil Roshan A <117465891+sunilroshan@users.noreply.github.com>
2025-05-11 13:14:25 +0530

    Time: 42 ms (84.68%) | Memory: 55.1 MB (63.53%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.java
+++ 940-fruit-into-baskets/fruit-into-baskets.java
@@ -18,41 +18,41 @@ class Solution {
         //         else{
         //             break;
         //         }
-                
+
         //     }
         // }
-
 
         //actual they are optimizing the code by the data structure
 
         int l = 0;
         int r = 0;
-        HashMap<Integer,Integer> map = new HashMap<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
 
-        while(r < fruits.length){
-            map.put(fruits[r],map.getOrDefault(fruits[r] , 0) + 1);
+        while (r < fruits.length) {
+            map.put(fruits[r], map.getOrDefault(fruits[r], 0) + 1);
 
-            while(map.size() > 2){
-                map.put(fruits[l],map.get(fruits[l]) - 1);
+            while (map.size() > 2) {
+                int a = map.get(fruits[l]);
+                a--;
 
-                if(map.get(fruits[l]) == 0){
+                if (a == 0) {
                     map.remove(fruits[l]);
+                } else {
+                    map.put(fruits[l], a);
                 }
 
                 l++;
 
             }
 
-            if(map.size() <= 2){
-                maxlen = Math.max(maxlen,r - l + 1);
+            if (map.size() <= 2) {
+                maxlen = Math.max(maxlen, r - l + 1);
                 r++;
             }
 
-
         }
-        
 
         return maxlen;
-        
+
     }
 }
